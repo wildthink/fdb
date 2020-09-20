da74ffb724a822fa122bdca3ae917c4f15686b46
Jason Jobe <box2019@jasonjobe.com>
2020-09-20 17:02:51 -0400

    package update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "/Users/jason/Development/frameworks/FeistyDB"),
+//        .package(path: "/Users/jason/Development/frameworks/FeistyDB"),
+        .package(url: "https://github.com/wildthink/FeistyDB", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
